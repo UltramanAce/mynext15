932738359b03177d30987d436a18a856bead6916
UltramanAce <2496638664@qq.com>
2025-03-14 17:35:11 +0800

    feat: 优化代码

--- src/app/client-components/fetchList.tsx
+++ src/app/client-components/fetchList.tsx
@@ -1,30 +1,39 @@
-'use client';
+'use client'
 
 import { useEffect, useState } from 'react';
 
 export default function FetchList() {
   const [listData, setListData] = useState<any[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch('../api/fetchList', {
+        const res = await fetch('/api/fetchList', {
           method: 'GET',
         });
-
         if (!res.ok) {
           throw new Error(`HTTP status: ${res.status}`);
         }
 
         const data = await res.json();
         setListData(data);
-      } catch (error) {
+      } catch (error: unknown) {
         console.error('Request error:', error);
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError('发生未知错误');
+        }
       }
     };
 
     fetchData();
   }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div>
