964230e04d408dbafc6106276ef0b8f45c61b6a8
UltramanAce <2496638664@qq.com>
2025-03-14 17:33:57 +0800

    feat: 代码优化

--- src/app/api/fetchList/route.ts
+++ src/app/api/fetchList/route.ts
@@ -1,32 +1,30 @@
-// api/fetchList.ts
-
 import fs from 'fs';
 import path from 'path';
-import type { NextApiRequest } from 'next';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-import { fetchAndSaveList } from '../../../utils/fetchList';
+import { fetchAndSaveListData } from '../../utils/fetchList';
 
-const OUTPUT_FILE = path.join(process.cwd(), 'lib', 'list.json');
+const OUTPUT_FILE = path.resolve('src/app/lib/data/list.json');
 
 // GET 请求处理
-export async function GET(req: NextApiRequest) {
+export async function GET(req: NextRequest) {
   try {
     // 调用抓取和保存列表数据的函数
-    // await fetchAndSaveList();
+    await fetchAndSaveListData();
 
     // 读取本地 JSON 文件并返回数据
-    // const listData = JSON.parse(fs.readFileSync(OUTPUT_FILE, 'utf-8'));
+    const listData = JSON.parse(fs.readFileSync(OUTPUT_FILE, 'utf-8'));
 
-    // NextResponse.json(listData, { status: 200 });
-    return NextResponse.json([], { status: 200 });
+    return NextResponse.json(listData, {
+      status: 200,
+    });
   } catch (error) {
-    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json({ message: 'Internal Server Error111' }, { status: 500 });
   }
 }
 
 // POST 请求处理
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const body = await request.json();
   return NextResponse.json({ received: body });
 }
